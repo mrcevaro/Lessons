23d44ba52ed7783b57b217abbf10b991173e79cc
mrcevaro <mrcevaro@github.com>
2020-02-23 15:44:05 +0300

    Add lewsson3 tasks

--- Lesson3/Lesson3/Sorts/Arrays.h
+++ Lesson3/Lesson3/Sorts/Arrays.h
@@ -127,3 +127,43 @@ int GetCountOfMax(int* arr, int n)
 	}
 	return count_last_max_element;
 }
+
+
+
+int Task_1_6_AlmostMax(int* arr, int n)
+{
+	// Задача 1.6. Почти максимум
+		// Функция получает на вход массив и его размер.
+		// Вернуть число которое было бы максимальным, если бы не было максимального.
+		// Иными словами, предмаксимум
+		//	test.ExpectEq({ 2,9,9,4,7,5,6,9,3,1,9,7,4 }, 7); // Максимум тут 9, следом за ним идет число 7
+	//test.ExpectEq({ 1,2,3,4,3,2,1 }, 3);
+
+	if (n == 0)
+	{
+		return -1;
+	}
+
+	int index_max_element = 0;
+	int index_prev_max_element = -1;
+	int max_element = arr[0];
+
+	int prev_max_element = arr[0];
+
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] > max_element)
+		{
+			max_element = arr[i];
+		}
+		if (arr[i] < max_element)
+		{
+			if (arr[i] > prev_max_element)
+			{
+				prev_max_element = arr[i];
+			}
+		}
+	}
+	return prev_max_element;
+}
+
