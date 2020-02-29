a82ca5f4c8a3c56aa15f55ac7da3f133d0f0043d
mrcevaro <mrcevaro@github.com>
2020-02-29 17:40:07 +0300

    Update AdditionalTask

--- Additional/Additional/Tasks_1.h
+++ Additional/Additional/Tasks_1.h
@@ -56,29 +56,42 @@ void Task1_4(int* arr, int n)
 {
 	/*1.4.Дан массив размера n.В массиве могут быть только числа 0, 1, 2, 3, 4. Вывести сколько раз встречается каждое число.
 		Привем ввода : 3 2 3 4 0 0 0 2 4 3
-		Вывод : 0 - 3
+		Вывод : 
+		0 - 3
 		1 - 0
 		2 - 2
 		3 - 3
-		4 - 2*/
+		4 - 2
+		
+	*/
 
-	int numbers[5] = { 0, 1, 2, 3, 4 };
-	int counts[5] = {};
+	// { 1, 1`000`000, 1`000`000,2 }
+	/*
+		1 - 1
+		1000000 - 2
+		2 - 1
+	*/
+
+
+
+	int counts[5] = {0};
+	
+	
+	//O(n);
+	// n
 
 	for (int i = 0; i < n; i++)
 	{
-		for (int z = 0; z < 5; z++) 
-		{
-			if (arr[i] == numbers[z]) 
-			{
-				counts[z]++;
-			}
-		}
+		const int z = arr[i];
+		counts[z]++;
 	}
-
+	
 	for (int i = 0; i < 5; i++)
 	{
-		std::cout << numbers[i] << " - " << counts[i] << std::endl;
+		if (counts[i] != 0)
+		{
+			std::cout << i << " - " << counts[i] << std::endl;
+		}
 	}
 
 	
