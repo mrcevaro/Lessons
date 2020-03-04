e87908c03da2e9f857f74a049e52a7a0fc8aaf15
mrcevaro <mrcevaro@github.com>
2020-03-04 22:30:34 +0300

    Add Lessons6

---
+++ Lesson6/Lesson6/Source.cpp
@@ -0,0 +1,89 @@
+#include <windows.h>
+#include <iostream>
+#include <conio.h>
+
+class ConsoleHelper
+{
+
+	HANDLE _console_handle;
+	CONSOLE_CURSOR_INFO info;
+public:
+
+	ConsoleHelper() {
+		_console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		info.bVisible = false;
+		info.dwSize = 100;
+		SetConsoleCursorInfo(_console_handle, &info);
+	}
+	void SetPosition(short x, short y)
+	{
+		SetConsoleCursorPosition(_console_handle, { x, y });
+	}
+	void Print(short x, short y, char ch)
+	{
+		SetPosition( x, y);
+		std::cout.put(ch);
+	}
+};
+
+
+int main() 
+{
+	ConsoleHelper cs;
+
+	int xx[10][10] = {0};
+
+	xx[3][4] = 1;
+	xx[3][5] = 1;
+	xx[3][7] = 1;
+	xx[3][9] = 1;
+
+	for (int i = 0; i <  4; i++)
+	{
+		cs.Print(x, y, '#');
+	}
+
+	int x = 3;
+	int y = 5;
+
+
+	if(xx[x][y] == 1)
+	{
+	}
+
+	cs.Print(x, y, '*');
+
+
+	/*while (true)
+	{
+		char ch = std::rand() % 255;
+		if (ch == '\a')
+		{
+			continue;
+		}
+
+		cs.Print(std::rand() % 50, std::rand() % 50, ch);
+	}*/
+	
+	
+
+	while (true)
+	{
+		char ch = _getch();
+		cs.Print(x, y, ' ');
+
+		switch (ch)
+		{
+		case 'w': y--; break;
+		case 'a': x--; break;
+		case 's': y++; break;
+		case 'd': x++; break;
+		}
+		
+		cs.Print(x, y, '*');
+	}
+	
+	//std::cout << ch << std::endl;
+
+	std::system("pause");
+}
